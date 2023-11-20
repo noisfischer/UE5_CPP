16369e5a5a62e3594cbccccb36081b333aecdc96
noisfischer <noisfischer@gmail.com>
2023-11-20 16:27:45 -0500

    Combines EaseMover, MyRotator, and Scaler scripts

--- Source/GDPTerm2Project/MasterTransform.cpp
+++ Source/GDPTerm2Project/MasterTransform.cpp
@@ -1,5 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Incorporates scripts from EaseMover, MyRotator, and Scaler
 
 #include "MasterTransform.h"
 
--- Source/GDPTerm2Project/MasterTransform.h
+++ Source/GDPTerm2Project/MasterTransform.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Incorporates scripts from EaseMover, MyRotator, and Scaler
 
 #pragma once
 
