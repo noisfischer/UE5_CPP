bf49596aeab68d4b4db0e981b2ddcad1d28dbf0e
noisfischer <noisfischer@gmail.com>
2023-11-20 16:26:20 -0500

    Projectile base class functionalities

--- Source/GDPTerm2Project/Projectile.cpp
+++ Source/GDPTerm2Project/Projectile.cpp
@@ -9,17 +9,18 @@ AProjectile::AProjectile()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	//Initialize Projectile Mesh
+	// Initialize the Projectile Mesh and set it as the Root Component.
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMesh"));
 	RootComponent = ProjectileMesh;
 
-	//Initialize the Projectile Movement Component
+	// Initialize the Projectile Movement Component with specific properties.
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
-	ProjectileMovementComponent->InitialSpeed = 3000.0f;
-	ProjectileMovementComponent->MaxSpeed = 3000.0f;
-	ProjectileMovementComponent->bRotationFollowsVelocity = true;
-	ProjectileMovementComponent->bShouldBounce = false;
-
+	ProjectileMovementComponent->InitialSpeed = 3000.0f; // Set the initial speed of the projectile.
+	ProjectileMovementComponent->MaxSpeed = 3000.0f; // Set the maximum speed of the projectile.
+	ProjectileMovementComponent->bRotationFollowsVelocity = true; // The projectile's rotation will follow its velocity.
+	ProjectileMovementComponent->bShouldBounce = false; // The projectile will not bounce upon impact.
+	
+	// Set the damage the projectile can inflict.
 	Damage = 5;
 
 }
@@ -36,16 +37,18 @@ void AProjectile::Tick(float DeltaTime)
 
 }
 
-
+// NotifyHit is called when the projectile hits something.
 void AProjectile::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
 {
-	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit); // Call base class's NotifyHit
 
 	if(Other != nullptr)
 	{
+		// Apply damage to the hit actor.
 		UGameplayStatics::ApplyPointDamage(Other, Damage, GetActorLocation() - Other->GetActorLocation(), Hit, nullptr, this, nullptr);
 	}
 
+	// Destroy the projectile after it hits something.
 	Destroy();
 }
 
