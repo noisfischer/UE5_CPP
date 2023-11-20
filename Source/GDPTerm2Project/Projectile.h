d9923c7449bddfe1dacd2bcfb8c7a90622351c7e
noisfischer <noisfischer@gmail.com>
2023-11-20 16:24:12 -0500

    Define projectile properties

--- Source/GDPTerm2Project/Projectile.h
+++ Source/GDPTerm2Project/Projectile.h
@@ -16,15 +16,19 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	// Static Mesh Component to visually represent the projectile.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
 	UStaticMeshComponent* ProjectileMesh;
 
+	// Movement Component for handling projectile movement.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
 	UProjectileMovementComponent* ProjectileMovementComponent;
 
+	// Damage property of the projectile.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
 	int Damage;
 
+	// Override for NotifyHit to handle behavior when the projectile hits something.
 	virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
 	
 	
