a771b1805d34f40e9d7c328e72a67ae9839a1b19
noisfischer <noisfischer@gmail.com>
2023-11-20 15:57:36 -0500

    Define smooth rotation variables and functions

--- Source/GDPTerm2Project/MyRotator.h
+++ Source/GDPTerm2Project/MyRotator.h
@@ -21,31 +21,32 @@ protected:
 	virtual void BeginPlay() override;
 
 private:
-	FRotator StartRotation;
-	FRotator CurrentRotation;
-	FRotator TargetRotation;
+	// Internal rotation values used for rotation logic.
+	FRotator StartRotation; // Initial rotation of the component.
+	FRotator CurrentRotation; // Current rotation of the component.
+	FRotator TargetRotation; // Target rotation to reach.
 
 	UPROPERTY(EditAnywhere, Category="Rotate", meta=(ClampMin="0", ClampMax="20", UIMin="0", UIMax="20"))
-	float RotationTime = 5.0f;  // default value
+	float RotationTime = 5.0f;  // Total time for the rotation to complete.
 
 	UPROPERTY(EditAnywhere, Category="Rotate")
-	FRotator RotateOffset;
+	FRotator RotateOffset; // Target rotation
 
 	// Current elapsed time since rotation started
 	float CurrentTime = 0.0f;
 
 	UPROPERTY(EditAnywhere, Category="Rotate")
-	bool BRotateReverse;
+	bool BRotateReverse; // Flag for reversing the rotation.
 
 	UPROPERTY(EditAnywhere, Category="Rotate")
-	bool BContinuousRotation;
+	bool BContinuousRotation; // Flag for continuous rotation.
 
-	// Private Functions
-	void MasterRotate(float DeltaTime);
-	void FRotateForward(float DeltaTime);
-	void FRotateReverse(float DeltaTime);
-	void FContinuousRotate(float DeltaTime);
-	void ApplyRotation(float DeltaTime);
-	float CubicEaseInOut(float t);
+	// Private Functions for rotation logic
+	void MasterRotate(float DeltaTime); // Master function to determine rotation type.
+	void FRotateForward(float DeltaTime); // Function to handle forward rotation.
+	void FRotateReverse(float DeltaTime); // Function to handle reverse rotation.
+	void FContinuousRotate(float DeltaTime); // Function to handle continuous rotation.
+	void ApplyRotation(float DeltaTime); // Applies the calculated rotation to the actor.
+	float CubicEaseInOut(float t); // Easing function for smooth rotation.
 };
 
