c30414264a1c38bf58749085f101f1a505d67ef9
noisfischer <noisfischer@gmail.com>
2023-11-20 15:39:44 -0500

    Explosive component functionalities

--- Source/GDPTerm2Project/ExplodingBarrelComponent.cpp
+++ Source/GDPTerm2Project/ExplodingBarrelComponent.cpp
@@ -9,13 +9,16 @@ UExplodingBarrelComponent::UExplodingBarrelComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 
+	// Initial values for the barrel's properties.
 	HitPoints = 10.0f;
 	ExplosionDamage = 5.0f;
 	ExplosionRadius = 20.0f;
 
+	// Initialize and configure the fire particle system component
 	FireParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("FireParticleSystemComponent"));
 	FireParticleSystemComponent->bAutoActivate = false;
 
+	// Initialize and configure the radial force component.
 	RadialForce = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForce"));
 	RadialForce->Radius = ExplosionRadius;
 	RadialForce->bImpulseVelChange = true;
@@ -23,11 +26,13 @@ UExplodingBarrelComponent::UExplodingBarrelComponent()
 	RadialForce->bIgnoreOwningActor = false;
 }
 
+// Called when the game starts or when spawned.
 void UExplodingBarrelComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	if (GetOwner())
 	{
+		// Register the OnDamageTaken function to respond when the actor takes any damage.
 		GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UExplodingBarrelComponent::OnDamageTaken);
 	}
 }
@@ -37,18 +42,22 @@ void UExplodingBarrelComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
+// Handler for when the barrel takes damage.
 void UExplodingBarrelComponent::OnDamageTaken(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
 {
 	if(Damage > 0.f && HitPoints > 0.f)
 	{
+		// Decrease hit points by the damage amount.
 		HitPoints -= Damage;
 		UE_LOG(LogTemp, Warning, TEXT("Damage taken, Hitpoints: %f"), HitPoints);
 
+		// Activate the fire particle system.
 		if (FireParticleSystemComponent != nullptr)
 		{
 			FireParticleSystemComponent->Activate();
 		}
 
+		// Start a countdown timer if it's not already active.
 		if (!GetWorld()->GetTimerManager().IsTimerActive(CountdownTimerHandle))
 		{
 			UE_LOG(LogTemp, Warning, TEXT("Setting countdown timer"));
@@ -56,28 +65,41 @@ void UExplodingBarrelComponent::OnDamageTaken(AActor* DamagedActor, float Damage
 		}
 	}
 
-	if (HitPoints <= 0.f)
+	// Check if the barrel's hit points have dropped to zero and it hasn't been destroyed yet.
+	if (HitPoints <= 0.f && !bHasBeenDestroyed)
 	{
+		bHasBeenDestroyed = true;
+		
+		// Deactivate the fire particle system.
 		if (FireParticleSystemComponent != nullptr)
 		{
 			FireParticleSystemComponent->Deactivate();
 		}
 
+		// Apply radial damage and effects.
 		FVector BarrelLocation = GetOwner()->GetActorLocation();
 		UGameplayStatics::ApplyRadialDamage(GetOwner(), ExplosionDamage, BarrelLocation, ExplosionRadius, nullptr, TArray<AActor*>(), GetOwner());
 
+		// Trigger radial force impulse.
 		if (RadialForce)
 		{
 			RadialForce->FireImpulse();
 		}
 
+		// Spawn explosion effects.
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionParticle, BarrelLocation);
 		UGameplayStatics::PlaySoundAtLocation(GetWorld(), ExplosionSound, BarrelLocation);
 
-		GetOwner()->Destroy();
+		// I don't want to use this for my game's specific respawn system
+		/*  GetOwner()->Destroy();  */
+		
+		// Set actor to be invisible and have no collisions
+		GetOwner()->SetActorHiddenInGame(true);
+        GetOwner()->SetActorEnableCollision(false);
 	}
 }
 
+// Countdown function, reducing hit points over time.
 void UExplodingBarrelComponent::Countdown()
 {
 	HitPoints--;
