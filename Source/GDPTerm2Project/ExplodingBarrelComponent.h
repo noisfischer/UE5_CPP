cc271cfbd580633d73b9f0da2a14d01f5b5a0853
noisfischer <noisfischer@gmail.com>
2023-11-20 15:35:42 -0500

    Variables and functions defined for explosive component

--- Source/GDPTerm2Project/ExplodingBarrelComponent.h
+++ Source/GDPTerm2Project/ExplodingBarrelComponent.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
-#include "Sound/SoundCue.h"
-#include "PhysicsEngine/RadialForceComponent.h"
-#include "Particles/ParticleSystemComponent.h"
+#include "CoreMinimal.h" // Core functionalities.
+#include "Components/ActorComponent.h" // Base class for actor components.
+#include "Sound/SoundCue.h" // For sound cue functionalities.
+#include "PhysicsEngine/RadialForceComponent.h" // For radial force physics.
+#include "Particles/ParticleSystemComponent.h" // For particle system components.
 #include "ExplodingBarrelComponent.generated.h"
 
 UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -21,36 +21,39 @@ protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	UStaticMeshComponent* BarrelMesh;
+	UStaticMeshComponent* BarrelMesh; //static mesh component of exploding barrel
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exploding Barrel")
-	float HitPoints;
+	float HitPoints; //health of component
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exploding Barrel")
-	float ExplosionDamage;
+	float ExplosionDamage; //radial damage dealt
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exploding Barrel")
-	float ExplosionRadius;
+	float ExplosionRadius; // area of effect of explosion
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Exploding Barrel")
-	USoundCue* ExplosionSound;
+	USoundCue* ExplosionSound; //modifiable explosion sound
 
 	UPROPERTY(EditAnywhere, Category = "Effects")
-	UParticleSystem* ExplosionParticle;
+	UParticleSystem* ExplosionParticle; //modifiable explosion effect
 
 	UPROPERTY(VisibleAnywhere, Category = "Effects")
-	UParticleSystemComponent* FireParticleSystemComponent;
+	UParticleSystemComponent* FireParticleSystemComponent; //modifiable fire particle effect
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Effects")
-	URadialForceComponent* RadialForce;
+	URadialForceComponent* RadialForce; //for physics interactions like enemy ragdoll on death
 
+	// BlueprintCallable function to handle damage taken by the barrel.
 	UFUNCTION(BlueprintCallable)
 	void OnDamageTaken(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
 
+	// Explosion timer countdown when barrel is slightly damaged
 	FTimerHandle CountdownTimerHandle;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Destroyed")
-	bool bHasBeenDestroyed = false;
+	bool bHasBeenDestroyed = false; //Check if actor with explosive component has been destroyed
 
+	// Function for countdown behavior.
 	void Countdown();
 };
