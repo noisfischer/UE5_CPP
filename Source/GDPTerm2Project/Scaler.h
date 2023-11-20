4ea319328ea0a33cbf6fedc938e25f700a522c6f
noisfischer <noisfischer@gmail.com>
2023-11-20 16:18:05 -0500

    Defines smooth scaler variables and functions

--- Source/GDPTerm2Project/Scaler.h
+++ Source/GDPTerm2Project/Scaler.h
@@ -19,27 +19,34 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-	FVector StartScale;
-	FVector TargetScale;
-	FVector CurrentScale;
+	// Internal scaling values used for scaling logic.
+	FVector StartScale; // Initial scale of the component.
+	FVector TargetScale; // Target scale to reach.
+	FVector CurrentScale; // Current scale of the component.
 
+	// Main function to determine scaling type.
 	void MasterScale(float DeltaTime);
+	// Function for smoothstep interpolation: Provides ease-in and ease-out scaling.
 	FVector SmoothstepInterp(const FVector& Current, const FVector& Target, float Alpha);
+	// Function to handle scaling up.
 	void FScaleUp(float DeltaTime);
+	// Function to handle scaling down.
 	void FScaleDown(float DeltaTime);
+	// Function to handle continuous scaling.
 	void FContinuousScale(float DeltaTime);
 
+	// Scaling properties.
 	UPROPERTY(EditAnywhere, Category = "Scaling")
-	FVector ScaleOffset;
+	FVector ScaleOffset; // Target scale
 
 	UPROPERTY(EditAnywhere, Category = "Scaling")
-	float ScaleTime;
+	float ScaleTime; // Total time for the scaling to complete.
 
 	UPROPERTY(EditAnywhere, Category = "Scaling")
-	bool bScaleReverse;
+	bool bScaleReverse; // Flag for reversing the scaling.
 
 	UPROPERTY(EditAnywhere, Category = "Scaling")
-	bool bContinuousScaling;
+	bool bContinuousScaling; // Flag for continuous scaling.
 
-	float StartTime;
+	float StartTime; // Time when the scaling started.
 };
